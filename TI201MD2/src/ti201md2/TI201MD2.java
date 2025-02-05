3bf8a0e27636a024ce19d89d0bff1fa572add264
frajavimopu <frajavimopu@gmail.com>
2025-02-05 14:09:12 -0600

    inicio de repositorio MD2

---
+++ TI201MD2/src/ti201md2/TI201MD2.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ti201md2;
+
+/**
+ *
+ * @author TICS03
+ */
+public class TI201MD2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
